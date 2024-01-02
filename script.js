9e54abb27516a86b4253930148f392a5f974132f
hautalaservice <155441264+hautalaservice@users.noreply.github.com>
2024-01-02 14:34:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Function to update score and color based on user input
-function updateScore(category, score) {
-    let element = document.getElementById(category);
-    // Remove all color classes before adding new one
-    element.classList.remove('deep-red', 'light-red', 'grey', 'light-green', 'deep-green');
-    colorGridItem(category, score);
-}
-
 // Function to apply color based on score
 function colorGridItem(category, score) {
     let element = document.getElementById(category);
+    // Remove all color classes before adding new one
+    element.classList.remove('deep-red', 'light-red', 'grey', 'light-green', 'deep-green');
     switch (score) {
         case '1': element.classList.add('deep-red'); break;
         case '2': element.classList.add('light-red'); break;
@@ -18,3 +12,16 @@ function colorGridItem(category, score) {
         default: element.classList.add('grey'); // default color if score is not 1-5
     }
 }
+
+// Function to update score and color based on user input
+function updateScore(category, score) {
+    colorGridItem(category, score);
+}
+
+// Initialize colors based on the default value
+document.addEventListener('DOMContentLoaded', function () {
+    updateScore('Johto', '3');
+    updateScore('Huolto', '3');
+    updateScore('Myynti', '3');
+    updateScore('Logistiikka', '3');
+});
